4a2751e36b3871edf2983d57930b92e47e77feca
Ryan Martin <eqpoqpe@gmail.com>
2024-04-07 08:13:16 +0800

    fix side bar

--- src/components/marketing/drop-image/side-bar.tsx
+++ src/components/marketing/drop-image/side-bar.tsx
@@ -18,30 +18,32 @@ export function SideBar(props: SideBarProps) {
         className={twJoin(
           "flex",
           "flex-row",
-          "bg-white/75",
+          "bg-black/75",
           "backdrop-blur-lg",
-          "mb-2",
+          "mb-5",
           "box-border",
           "py-3",
           "border",
           "border-solid",
-          "border-neutral-200",
+          "border-neutral-800",
           "rounded-xl",
           "box-border"
         )}
       >
         <CrossCircledIcon
-          className={twJoin("mx-3", "cursor-pointer")}
+          className={twJoin("mx-3", "cursor-pointer", "text-neutral-300")}
           onClick={onClose}
         />
         <Share2Icon
           className={twJoin(
             "mx-3",
             true && "cursor-not-allowed",
-            true && "text-neutral-300"
+            true && "text-neutral-700"
           )}
         />
-        <InfoCircledIcon className={twJoin("mx-3")} />
+        <InfoCircledIcon
+          className={twJoin("mx-3", "cursor-pointer", "text-neutral-300")}
+        />
       </div>
     </div>
   );
